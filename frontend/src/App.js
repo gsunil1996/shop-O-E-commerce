ad9e3fed637f4ccf7fe8709d55b86c4665872cb7
Sunil Kumar <gajulasunilkumar123@gmail.com>
2023-06-14 08:56:26 +0530

    backend code added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PageNotFound from './pages/PageNotFound';
+// user routes
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
-    <div>
-      app
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
